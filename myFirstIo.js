a09c0ec80d385568eaded27b9ffcae86d7e71d0a
George Smith-Sweeper <george.smsweeper@gmail.com>
2016-07-15 10:32:06 -0400

    complete

--- myFirstIo.js
+++ myFirstIo.js
@@ -1,14 +1,11 @@
 var fs = require('fs');
-
+// output from buffer converted into a string and split by newline chars
 var buffList = fs.readFileSync(process.argv[2]).toString().split("\n");
-//console.log(buffList);
-
 
 var counter = 0;
 
 for (var i = 1; i < buffList.length; i++) {
-	counter++
+	counter++;
 };
 
 console.log(counter);
-//console.log(process.argv);
